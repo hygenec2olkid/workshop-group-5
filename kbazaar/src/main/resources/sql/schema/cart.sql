f42f7874b9390966e5c5c04f313ea3104eb4e452
pongsakon promma <pongsakonpromma@gmail.com>
2024-03-23 14:59:01 +0700

    feat : initial sql cart

---
+++ kbazaar/src/main/resources/sql/schema/cart.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS cart (
+	shopper_id bigserial NOT NULL,
+	product_id bigserial NOT NULL,
+	quantity int4 NOT NULL,
+	product_sku varchar(255) NOT NULL,
+	CONSTRAINT cart_pk PRIMARY KEY (product_id, shopper_id)
+);
+CREATE INDEX cart_shopper_id_idx ON public.cart USING btree (shopper_id);
