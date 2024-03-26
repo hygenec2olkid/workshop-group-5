b702129029fe4a6d50f1f649b21b2be06ed090aa
wirapat.g <wirapat@sirisoft.co.th>
2024-03-26 16:31:21 +0700

    add gitgnore

--- kbazaar/src/main/resources/sql/schema/shopper.sql
+++ kbazaar/src/main/resources/sql/schema/shopper.sql
@@ -3,3 +3,23 @@ CREATE TABLE IF NOT EXISTS shopper (
 	username VARCHAR (255) UNIQUE NOT NULL,
 	email VARCHAR (255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS cart (
+	    id SERIAL PRIMARY KEY,
+    	user_id INT NOT NULL,
+    	discount_id INT,
+    	total DECIMAL(10, 2) NOT NULL,
+    	discount DECIMAL(10, 2) DEFAULT 0.00
+);
+
+ALTER TABLE cart ADD CONSTRAINT cart_promotion_fk FOREIGN KEY (discount_id) REFERENCES promotion(promotion_id);
+ALTER TABLE cart ADD CONSTRAINT cart_shopper_fk FOREIGN KEY (user_id) REFERENCES shopper(id);
+
+CREATE TABLE IF NOT EXISTS cart_item (
+    id SERIAL PRIMARY KEY,
+    cart_session INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    CONSTRAINT cart_item_cart_fk FOREIGN KEY (cart_session) REFERENCES cart(id),
+    CONSTRAINT cart_item_product_fk FOREIGN KEY (product_id) REFERENCES product(id)
+);
