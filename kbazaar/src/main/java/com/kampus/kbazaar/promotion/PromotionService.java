b7a2c3e2780673cf0f25d66735fd249f47abba46
wirapat.g <wirapat@sirisoft.co.th>
2024-04-10 23:53:17 +0700

    feat: story 5 IMP

--- kbazaar/src/main/java/com/kampus/kbazaar/promotion/PromotionService.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/promotion/PromotionService.java
@@ -1,6 +1,10 @@
 package com.kampus.kbazaar.promotion;
 
+import com.kampus.kbazaar.cart.Cart;
+import com.kampus.kbazaar.cart.CartRepository;
 import com.kampus.kbazaar.exceptions.NotFoundException;
+import com.kampus.kbazaar.shopper.Shopper;
+import com.kampus.kbazaar.shopper.ShopperRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +12,17 @@ import org.springframework.stereotype.Service;
 public class PromotionService {
     private PromotionRepository promotionRepository;
 
-    public PromotionService(PromotionRepository promotionRepository) {
+    private ShopperRepository shopperRepository;
+
+    private CartRepository cartRepository;
+
+    public PromotionService(
+            PromotionRepository promotionRepository,
+            ShopperRepository shopperRepository,
+            CartRepository cartRepository) {
         this.promotionRepository = promotionRepository;
+        this.shopperRepository = shopperRepository;
+        this.cartRepository = cartRepository;
     }
 
     public List<PromotionResponse> getAll() {
@@ -24,6 +37,25 @@ public class PromotionService {
     }
 
     public String usePromotionCode(String username, String code) {
-        return "test";
+        Shopper shopper =
+                this.shopperRepository
+                        .findByUsername(username)
+                        .orElseThrow(
+                                () ->
+                                        new NotFoundException(
+                                                "Shopper "
+                                                        + username
+                                                        + " not have in ShopperRepo"));
+
+        Cart cart =
+                this.cartRepository
+                        .findByShopper_Id(shopper.getId())
+                        .orElseThrow(
+                                () ->
+                                        new NotFoundException(
+                                                "Can't use promotion code because "
+                                                        + username
+                                                        + " not have item in cart"));
+        return cart.getShopper().getUsername();
     }
 }
--- kbazaar/src/test/java/com/kampus/kbazaar/promotion/PromotionServiceTest.java
+++ kbazaar/src/test/java/com/kampus/kbazaar/promotion/PromotionServiceTest.java
@@ -3,7 +3,10 @@ package com.kampus.kbazaar.promotion;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.kampus.kbazaar.cart.CartRepository;
 import com.kampus.kbazaar.exceptions.NotFoundException;
+import com.kampus.kbazaar.shopper.Shopper;
+import com.kampus.kbazaar.shopper.ShopperRepository;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +20,8 @@ import org.mockito.MockitoAnnotations;
 class PromotionServiceTest {
 
     @Mock private PromotionRepository promotionRepository;
+    @Mock private ShopperRepository shopperRepository;
+    @Mock private CartRepository cartRepository;
 
     @InjectMocks private PromotionService promotionService;
 
@@ -66,5 +71,41 @@ class PromotionServiceTest {
 
         // Act & Assert
         assertThrows(NotFoundException.class, () -> promotionService.getPromotionByCode(code));
+    }
+
+    @Test
+    @DisplayName("should throw NotFoundException when can't find shopper")
+    public void shouldThrowNotFoundExceptionWhenNotFoundShopper() {
+        String username = "test";
+        String promotionCode = "TEST-CODE-1";
+        String expected = "Shopper " + username + " not have in ShopperRepo";
+        when(shopperRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        Exception actual =
+                assertThrows(
+                        NotFoundException.class,
+                        () -> promotionService.usePromotionCode(username, promotionCode));
+
+        assertEquals(expected, actual.getMessage());
+    }
+
+    @Test
+    @DisplayName("should throw NotFoundException when can't find cart of shopper")
+    public void shouldThrowNotFoundExceptionWhenNotFoundCartOfShopper() {
+        String username = "test";
+        String promotionCode = "TEST-CODE-1";
+        String expected = "Can't use promotion code because " + username + " not have item in cart";
+
+        Shopper shopper = new Shopper();
+        shopper.setId(1L);
+        when(shopperRepository.findByUsername(username)).thenReturn(Optional.of(shopper));
+        when(cartRepository.findByShopper_Id(shopper.getId())).thenReturn(Optional.empty());
+
+        Exception actual =
+                assertThrows(
+                        NotFoundException.class,
+                        () -> promotionService.usePromotionCode(username, promotionCode));
+
+        assertEquals(expected, actual.getMessage());
     }
 }
